2774b2e2b44df03f285c7f49a58f598f89fd8e1e
L0c_d0G <94112336+ruslanbadaev01@users.noreply.github.com>
2022-04-26 17:09:27 +0300

    cat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { VideoCardComponent } from './video-card/video-card.component';
+import { CardPageComponent } from './card-page/card-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    VideoCardComponent,
+    CardPageComponent
   ],
   imports: [
     BrowserModule
